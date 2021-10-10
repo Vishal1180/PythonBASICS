3a0b4cb9c432328fe0e91eb2f9f0498c177b06e2
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-10-10 13:20:46 +0530

    AREA OF Circle py file
    Py file for Calculating Area of circle

---
+++ AreaCircle.py
@@ -0,0 +1,10 @@
+#Write a program to find diameter, circumference and area of circle.
+from math import pi
+radius = float(input("Enter the value of radius:"))
+Diameter = 2 * radius
+Cir = 2 * pi
+Area = pi * Diameter
+
+print("Diameter of circle is:",Diameter)
+print("Circumference of circle is:",Cir)
+print("Area of circle is:",Area)
