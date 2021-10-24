1277bae94091c921794fb541bfbcbecc9339d24d
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-10-24 09:53:16 +0530

    Pycode File
    Inheritance Explained

---
+++ College_Project_Inheritance.py
@@ -0,0 +1,25 @@
+print('\t\tWELCOME TO Govt.College Enginnering\n\t\t\t\t\tAmravati')
+print('Estd: 1962  College Code 1101   A+ NAAC Accredited')
+class College:
+    def __init__(self):
+        pass
+    def admin(self,Chairman='Mr.Bean Lee',Secratory='Mr.JK Lee'):
+        self.Chairman= Chairman
+        self.Secratory= Secratory
+    def principal(name,salary):
+        pass
+
+class Department(College):
+    pass
+class CSE(Department):
+    pass
+class IT(Department):
+    pass
+class AI(Department):
+    pass
+class Staff:
+    pass
+class Student:
+    pass
+s1 = Student()
+s2 = Staff()
