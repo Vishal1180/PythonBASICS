a54c876fefe749c0abd06dd9e1d57ccf07e02fc5
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-10-31 20:19:53 +0530

    py code file

---
+++ ReverseTuple.py
@@ -0,0 +1,12 @@
+#create a tuple
+x = ("w3resource")
+# Reversed the tuple
+y = reversed(x)
+print(tuple(y))
+#create another tuple
+x = (5, 10, 15, 20)
+# Reversed the tuple
+y = reversed(x)
+print(tuple(y))
+x1 = (3,4,5,6,7)
+print(x1[::-1])
