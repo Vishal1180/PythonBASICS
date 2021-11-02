c5d65573eee699bc906a7c67e48d71a59f5dd715
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-02 12:41:03 +0530

    py code file

---
+++ UnpackingTuple.py
@@ -0,0 +1,9 @@
+# Program to unpack the tuple
+#create a tuple
+tuplex = 4, 8, 3
+print(tuplex)
+n1, n2, n3 = tuplex
+#unpack a tuple in variables
+print(n1 + n2 + n3)
+#the number of variables must be equal to the number of items of the tuple
+n1, n2, n3 = tuplex
