0b556e2a31b562c6a9b3dd14dc6a21f861438cab
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-04 20:47:28 +0530

    PyCodefile

---
+++ DictMerge2.py
@@ -0,0 +1,7 @@
+# Write a Python program to concatenate following dictionaries to create a new one.
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic4 = {}
+for d in (dic1, dic2, dic3): dic4.update(d)
+print(dic4)
