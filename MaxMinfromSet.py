a99107035a3d0092490fac1f5764604a7ae324b0
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-08 22:18:37 +0530

    set exe2

---
+++ MaxMinfromSet.py
@@ -0,0 +1,9 @@
+# Maximum and Minimum in a Set
+# Python code to get the maximum element from a set
+def MAX(sets):
+    return (max(sets))
+def Min(sets):
+    return (min(sets))
+sets = set([8, 16, 24, 1, 25, 3, 10, 65, 55])
+print(MAX(sets))
+print(Min(sets))
