815d911ae54e9d2c0004ffbded268a39e500bca4
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-09 16:07:35 +0530

    SET Exe4

---
+++ AddMember2Set.py
@@ -0,0 +1,10 @@
+# Write a Python program to add member(s) in a set.
+#A new empty set
+color_set = set()
+print(color_set)
+print("\nAdd single element:")
+color_set.add("Red")
+print(color_set)
+print("\nAdd multiple items:")
+color_set.update(["Blue", "Green"])
+print(color_set)
