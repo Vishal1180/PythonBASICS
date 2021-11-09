95438888c647908b058780be5990cb3e9cd232b4
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-09 16:08:24 +0530

    SET exe6

---
+++ RemoveItem4mset.py
@@ -0,0 +1,7 @@
+# Remove item from set
+num_set = {0, 1, 3, 4, 5}
+print("Original set:")
+print(num_set)
+num_set.pop()
+print("\nAfter removing the first element from the said set:")
+print(num_set)
