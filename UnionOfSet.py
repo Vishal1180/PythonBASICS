5dc2c1cc236b80c8520f146e200b0a8fea09ca76
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-09 16:19:42 +0530

    SET EXE8

---
+++ UnionOfSet.py
@@ -0,0 +1,18 @@
+#WAP for union of two set
+
+setc1 = set(["green", "blue"])
+setc2 = set(["blue", "yellow"])
+print("Original sets:")
+print(setc1)
+print(setc2)
+setc = setc1.union(setc2)
+print("\nUnion of above sets:")
+print(setc)
+setn1 = set([1, 1, 2, 3, 4, 5])
+setn2 = set([1, 5, 6, 7, 8, 9])
+print("\nOriginal sets:")
+print(setn1)
+print(setn2)
+print("\nUnion of above sets:")
+setn = setn1.union(setn2)
+print(setn)
