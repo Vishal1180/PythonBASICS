08ed283624e7a55a57fda5983239ff45efb210ac
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-09 16:15:40 +0530

    SET Intersection

---
+++ SetInteraction.py
@@ -0,0 +1,9 @@
+# Write a Python program to create an intersection of sets.
+setx = (["green", "blue"])
+sety = (["blue", "yellow"])
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx and sety
+print(setz)
