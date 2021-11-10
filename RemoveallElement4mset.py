f102135a2cb5d6da5443bc2c58f565a2f7aaa331
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-10 13:44:58 +0530

    SET EXE

---
+++ RemoveallElement4mset.py
@@ -0,0 +1,7 @@
+# Write a Python program to remove all elements from a given set.
+setc = {"Red", "Green", "Black", "White"}
+print("Original set elements:")
+print(setc)
+print("\nAfter removing all elements of the said set.")
+setc.clear()
+print(setc)
