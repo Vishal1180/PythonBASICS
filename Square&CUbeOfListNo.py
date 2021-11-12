638b976c96ce30ef0efdba6da2619b97f22ff049
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-12 09:38:21 +0530

    Square&CubeOflistUsingLambda

---
+++ Square&CUbeOfListNo.py
@@ -0,0 +1,11 @@
+# Write a Python program to square and cube every number in a given list of integers using Lambda.
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Original list of integers:")
+print(nums)
+print("\nSquare every number of the said list:")
+square_nums = list(map(lambda x: x ** 2, nums))
+print(square_nums)
+print("\nCube every number of the said list:")
+cube_nums = list(map(lambda x: x ** 3, nums))
+print(cube_nums)
