c230c6ba5cff62cffaa91b675d85cdd24d247aa0
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-12 09:37:16 +0530

    DictSortUsingLambda

---
+++ DictSortUsingLambda.py
@@ -0,0 +1,8 @@
+# Write a Python program to sort a list of dictionaries using Lambda.
+
+models = [{'make':'Nokia', 'model':216, 'color':'Black'}, {'make':'Mi Max', 'model':'2', 'color':'Gold'}, {'make':'Samsung', 'model': 7, 'color':'Blue'}]
+print("Original list of dictionaries :")
+print(models)
+sorted_models = sorted(models, key = lambda x: x['color'])
+print("\nSorting the List of dictionaries :")
+print(sorted_models)
