75a76cdf12112bce53175a57a0f848c4f5be6f2a
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-12 09:38:48 +0530

    StringSTARTSwithLambda

---
+++ StringStartswith.py
@@ -0,0 +1,6 @@
+# Write a Python program to find whether a given string starts with a given character using Lambda.
+
+starts_with = lambda x: True if x.startswith('P') else False
+print(starts_with('Python'))
+starts_with = lambda x: True if x.startswith('P') else False
+print(starts_with('Java'))
