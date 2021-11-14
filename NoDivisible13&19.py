368cfab42a17274c704653a70faca8f6ff0a556a
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-14 19:00:27 +0530

    Lambda Function

---
+++ NoDivisible13&19.py
@@ -0,0 +1,8 @@
+# Write a Python program to find numbers divisible by nineteen or thirteen from a list of numbers using Lambda.
+
+nums = [19, 65, 57, 39, 152, 639, 121, 44, 90, 190]
+print("Orginal list:")
+print(nums)
+result = list(filter(lambda x: (x % 19 == 0 or x % 13 == 0), nums))
+print("\nNumbers of the above list divisible by nineteen or thirteen:")
+print(result)
