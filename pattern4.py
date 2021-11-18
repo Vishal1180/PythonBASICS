0e64fb4cb3e515e8a5b093e0888c488c258ec0ce
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-18 21:00:20 +0530

    PAttern Printing
    1
    12
    123
    1234

---
+++ pattern4.py
@@ -0,0 +1,15 @@
+n = int(input('enter no of rows:'))
+for i in range(n):# no of rows
+    for j in range(i+1): #column
+        print(j+1,end='')
+    print()
+'''
+n = int(input('enter no of rows:'))
+for i in range(n):# no of rows
+    for j in range(i,-1,-1): #column
+        print(j+1,end='')
+    print()
+
+
+
+'''
