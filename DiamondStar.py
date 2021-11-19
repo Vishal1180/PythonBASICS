a1e534cfea959306d6a737daee6a1764236cda47
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-19 13:06:07 +0530

    Diamond Pattern

---
+++ DiamondStar.py
@@ -0,0 +1,29 @@
+"""Diamond star pattern
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+"""
+# diamond star pattern
+n = 5
+
+# upward pyramid
+for i in range(n):
+    for j in range(n - i - 1):
+        print(' ', end='')
+    for j in range(2 * i + 1):
+        print('*', end='')
+    print()
+
+# downward pyramid
+for i in range(n - 1):
+    for j in range(i + 1):
+        print(' ', end='')
+    for j in range(2*(n - i - 1) - 1):
+        print('*', end='')
+    print()
