aacf929efa5283e51801154329d07a90ea4bffb0
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-21 17:18:58 +0530

    Pattern Printing Pyramid shape

---
+++ PyramidPattern.py
@@ -0,0 +1,15 @@
+"""Pyramid Pattern in python
+    *
+   ***
+  *****
+ *******
+*********
+"""
+# pyramid star pattern
+n = 5
+for i in range(n):
+    for j in range(n - i - 1):
+        print(' ', end='')
+    for k in range(2 * i + 1):
+        print('*', end='')
+    print()
