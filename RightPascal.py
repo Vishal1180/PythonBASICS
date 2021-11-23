b0ce208abce266633d82f873946d0002d50887bb
VISHAL PATIL <85478279+Vishal1180@users.noreply.github.com>
2021-11-23 19:48:31 +0530

    Pattern Program

---
+++ RightPascal.py
@@ -0,0 +1,18 @@
+"""Right pascal triangle pattern
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+"""
+# right pascal triangle
+n = 5
+
+for i in range(n):
+    print('*' * (i + 1))
+for i in range(n):
+    print('*' * (n - i - 1))
